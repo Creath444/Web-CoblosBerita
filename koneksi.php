dad1468c41174d3fc743b5818e4296013866c04f
Tampan Satria <72635816+Creath444@users.noreply.github.com>
2024-01-17 08:59:10 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,10 @@
 <?php 
-$koneksi = mysqli_connect("localhost","root","","web-pemilu");
+$DB_HOST = 'localhost';
+$DB_USER = 'nama_pengguna';
+$DB_PASS = 'kata_sandi';
+$DB_NAME = 'nama_database';
+
+$koneksi = mysqli_connect($DB_HOST,$DB_USER,$DB_PASS,$DB_NAME);
  
 // Check connection
 if (mysqli_connect_errno()){
